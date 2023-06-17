1bc24ebc5238d72b3c6a86e731194e45396eb7d5
AgaspherGames <agasphergg@gmail.com>
2023-06-17 18:34:23 +0600

    many many many work

---
+++ app/Models/Game.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Game extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'thumbnail',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function game_versions(){
+        return $this->hasMany(GameVersion::class);
+    }
+}
